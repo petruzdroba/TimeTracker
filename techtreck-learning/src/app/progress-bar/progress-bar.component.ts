f9adbc85789b0ded2105526eda026710dba273b1
petruzdrobantt <pezdroba@emeal.nttdata.com>
2025-02-19 11:50:39 +0200

    added time persistance

--- techtreck-learning/src/app/progress-bar/progress-bar.component.ts
+++ techtreck-learning/src/app/progress-bar/progress-bar.component.ts
@@ -1,24 +1,20 @@
-import { DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-progress-bar',
   standalone: true,
-  imports: [MatProgressBarModule, DatePipe],
+  imports: [MatProgressBarModule],
   templateUrl: './progress-bar.component.html',
   styleUrl: './progress-bar.component.sass',
 })
 export class ProgressBarComponent {
   @Input({ required: true }) elapsedTime!: number;
   @Input({ required: true }) timerType!: 'ON' | 'OFF';
-  requiredTime = 7200000;
+  @Input({ required: true }) requiredTime!: number;
+
   get valueStatus() {
     const timer = this.elapsedTime;
     return (timer / this.requiredTime) * 100;
-  }
-  get remainingTime() {
-    this.requiredTime -= this.elapsedTime;
-    return this.requiredTime;
   }
 }
--- techtreck-learning/src/app/timer/timer.component.ts
+++ techtreck-learning/src/app/timer/timer.component.ts
@@ -1,4 +1,4 @@
-import { DatePipe } from '@angular/common';
+import { DatePipe, JsonPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ProgressBarComponent } from '../progress-bar/progress-bar.component';
 
@@ -12,12 +12,34 @@ import { ProgressBarComponent } from '../progress-bar/progress-bar.component';
 export class TimerComponent implements OnInit {
   private startTime = new Date(0, 0, 0);
   private endTime = new Date(0, 0, 0);
-  timerType: 'ON' | 'OFF' = 'OFF';
+  protected requiredTime = 7200000;
+  protected timerType: 'ON' | 'OFF' = 'OFF';
 
   ngOnInit(): void {
-    //adauga local storage later
-    console.log(this.startTime.getHours());
-    console.log(this.endTime.getHours());
+    const storedStartTime = window.localStorage.getItem('startTime');
+    const storedEndTime = window.localStorage.getItem('endTime');
+    const storedRemainingTime = window.localStorage.getItem('remainingTime');
+
+    if (storedStartTime) {
+      this.startTime = new Date(JSON.parse(storedStartTime));
+    }
+
+    if (storedEndTime) {
+      this.endTime = new Date(JSON.parse(storedEndTime));
+    }
+
+    const currentTime = new Date();
+    const dateLastSession = new Date(this.startTime);
+    currentTime.setHours(0, 0, 0, 0);
+    dateLastSession.setHours(0, 0, 0, 0);
+    if (currentTime.getTime() === dateLastSession.getTime()) {
+      if (storedRemainingTime) {
+        this.requiredTime = JSON.parse(storedRemainingTime);
+      }
+    } else {
+      //resets if dates are different, 2 hours a day for each new day
+      this.requiredTime = 7200000;
+    }
   }
 
   get elapsedTime() {
@@ -36,12 +58,14 @@ export class TimerComponent implements OnInit {
       this.startTime = currentTime;
     } else {
       this.endTime = currentTime;
-      // console.log(
-      //   `START TIME ${this.startTime.getHours()}:${this.startTime.getMinutes()}`
-      // );
-      // console.log(
-      //   `END TIME ${this.endTime.getHours()}:${this.endTime.getMinutes()}`
-      // );
+      this.requiredTime -= this.elapsedTime;
+
+      window.localStorage.setItem('startTime', JSON.stringify(this.startTime));
+      window.localStorage.setItem('endTime', JSON.stringify(this.endTime));
+      window.localStorage.setItem(
+        'remainingTime',
+        JSON.stringify(this.requiredTime)
+      );
     }
   }
 }
