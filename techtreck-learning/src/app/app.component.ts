e8df921e065613a97a14a9eb11dac24ef331b3f9
petruzdrobantt <pezdroba@emeal.nttdata.com>
2025-02-21 12:06:12 +0200

    added basic nav bar

--- techtreck-learning/src/app/app.component.ts
+++ techtreck-learning/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TimerComponent } from './timer/timer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TimerComponent],
+  imports: [RouterOutlet, TimerComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
 })
---
+++ techtreck-learning/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [MatSidenavModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.sass',
+})
+export class NavBarComponent {
+  protected navBarStatus: 'OPEN' | 'CLOSE' = 'CLOSE';
+
+  onToggle() {
+    this.navBarStatus = this.navBarStatus === 'OPEN' ? 'CLOSE' : 'OPEN';
+  }
+}
