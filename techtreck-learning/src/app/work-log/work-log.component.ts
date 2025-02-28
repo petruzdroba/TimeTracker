7816e9fd477662ffdb7c688fb7f0fde6e9a66f9d
petruzdrobantt <pezdroba@emeal.nttdata.com>
2025-02-28 12:22:03 +0200

    started log

---
+++ techtreck-learning/src/app/work-log/work-log.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-log',
+  standalone: true,
+  imports: [],
+  templateUrl: './work-log.component.html',
+  styleUrl: './work-log.component.sass'
+})
+export class WorkLogComponent {
+
+}
