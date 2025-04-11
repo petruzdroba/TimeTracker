a14fbf4ecb8b43d581ccbad0ea602ad61f32d688
petruzdrobantt <pezdroba@emeal.nttdata.com>
2025-04-11 14:02:24 +0300

    finished lazy loading

---
+++ techtreck-learning/src/app/components/work-log/work-log.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { WorkLogComponent } from './work-log.component';
+
+export const WORKLOG_ROUTES: Routes = [
+  {
+    path: '',
+    component: WorkLogComponent,
+  },
+];
