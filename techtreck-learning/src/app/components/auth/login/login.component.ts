4fa6fc81184193b71266f7bf62750f79aeadfb58
Zdroba Petru <petru.zdroba@gmail.com>
2025-05-19 09:57:17 +0300

    log in component

--- techtreck-learning/src/app/components/auth/login/login.component.ts
+++ techtreck-learning/src/app/components/auth/login/login.component.ts
@@ -1,12 +1,53 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.sass'
+  styleUrl: './login.component.sass',
 })
 export class LoginComponent {
+  hidePassword = true;
 
+  protected form = new FormGroup({
+    email: new FormControl<string>('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl<string>('', {
+      validators: [Validators.required],
+    }),
+  });
+
+  onSubmit() {
+    if (this.form.invalid) {
+      console.log('INVALID FORM');
+      return;
+    }
+
+    console.log('Form submitted:', this.form.value);
+    // Handle form submission logic here, e.g., send data to the server
+  }
+
+  onReset() {
+    this.form.reset();
+  }
 }
