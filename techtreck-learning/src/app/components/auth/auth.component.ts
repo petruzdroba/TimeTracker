bc8bcedcc828f0453f4178053344d37c20159168
Zdroba Petru <petru.zdroba@gmail.com>
2025-05-27 09:45:59 +0300

    user info page

--- techtreck-learning/src/app/components/auth/auth.component.ts
+++ techtreck-learning/src/app/components/auth/auth.component.ts
@@ -1,17 +1,24 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { LoginComponent } from './login/login.component';
 import { SigninComponent } from './signin/signin.component';
+import { UserDataService } from '../../service/user-data.service';
+import { InfoComponent } from './info/info.component';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [MatTabsModule, LoginComponent, SigninComponent],
+  imports: [MatTabsModule, LoginComponent, SigninComponent, InfoComponent],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.sass',
 })
 export class AuthComponent {
   @Output() close = new EventEmitter<void>();
+  private userService = inject(UserDataService);
+
+  get isLoggedIn() {
+    return this.userService.isLoggedIn();
+  }
 
   closeWindow() {
     this.close.emit();
