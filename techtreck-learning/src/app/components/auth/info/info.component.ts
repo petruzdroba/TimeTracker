29e92754385bc0dae354180c04f8a1329cf7ddc1
Zdroba Petru <petru.zdroba@gmail.com>
2025-06-09 10:40:46 +0300

    FE component delete user

--- techtreck-learning/src/app/components/auth/info/info.component.ts
+++ techtreck-learning/src/app/components/auth/info/info.component.ts
@@ -10,11 +10,28 @@ import { UserDataService } from '../../../service/user-data.service';
 import { UserData } from '../../../model/user-data.interface';
 import { take } from 'rxjs';
 import { Router } from '@angular/router';
+import { EditBoxComponent } from '../../../shared/edit-box/edit-box.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [],
+  imports: [
+    EditBoxComponent,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+  ],
   templateUrl: './info.component.html',
   styleUrl: './info.component.sass',
 })
@@ -23,28 +40,52 @@ export class InfoComponent implements OnInit {
 
   private userService = inject(UserDataService);
   private router = inject(Router);
-
   protected userData = signal<UserData>({} as UserData);
+
+  protected showDialog: boolean = false;
+  protected form = new FormGroup({
+    password: new FormControl<string>('', {
+      validators: [Validators.required],
+    }),
+  });
+  protected accountDeleted: boolean = false;
 
   ngOnInit(): void {
     this.userData.set(this.userService.user());
   }
 
-  onDelete(password: string): void {
-    this.userService
-      .delete(password)
-      .pipe(take(1))
-      .subscribe({
-        next: (res) => {
-          this.userService.logout();
-        },
-        error: (err) => {
-          if (err.status === 401) {
-            this.userService.logout();
-          }
-          this.router.navigate(['/error', err.status]);
-        },
-      });
+  onShowDialog(): void {
+    this.showDialog = true;
+  }
+
+  onHideDialog(): void {
+    this.showDialog = false;
+  }
+
+  onDelete(): void {
+    if (this.form.value.password) {
+      this.userService
+        .delete(this.form.value.password)
+        .pipe(take(1))
+        .subscribe({
+          next: (res) => {
+            this.accountDeleted = true;
+            setTimeout(() => {
+              this.userService.logout();
+            }, 1500);
+          },
+          error: (err) => {
+            if (err.status === 401) {
+              setTimeout(() => {
+                this.userService.logout();
+              }, 500);
+            }
+            this.onHideDialog();
+            this.close.emit();
+            this.router.navigate(['/error', err.status]);
+          },
+        });
+    }
   }
 
   onLogout(): void {
