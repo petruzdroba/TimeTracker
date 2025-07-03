fc7bf209351ea001a8a28f65d2e7accb98c50e6e
Zdroba Petru <petru.zdroba@gmail.com>
2025-07-03 11:11:44 +0300

    slimmed down config

--- techtreck-learning/cypress.config.ts
+++ techtreck-learning/cypress.config.ts
@@ -1,39 +1,15 @@
 import { defineConfig } from 'cypress';
-import webpackPreprocessor from '@cypress/webpack-preprocessor';
 
 export default defineConfig({
+  component: {
+    devServer: {
+      framework: 'angular',
+      bundler: 'webpack',
+    },
+    specPattern: 'src/**/*.cy.ts',
+  },
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
     baseUrl: 'http://localhost:4200',
-    setupNodeEvents(on, config) {
-      // Wire in the Webpack preprocessor with your Cypress tsconfig
-      const options = {
-        webpackOptions: {
-          resolve: {
-            extensions: ['.ts', '.js'],
-          },
-          module: {
-            rules: [
-              {
-                test: /\.ts$/,
-                exclude: [/node_modules/],
-                use: [
-                  {
-                    loader: 'ts-loader',
-                    options: {
-                      configFile: 'cypress/tsconfig.json',
-                    },
-                  },
-                ],
-              },
-            ],
-          },
-        },
-        watchOptions: {},
-      };
-
-      on('file:preprocessor', webpackPreprocessor(options));
-      return config;
-    },
   },
 });
