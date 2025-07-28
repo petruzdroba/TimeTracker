ff6008e265e0285ad63c1db8e8a6f0c1a32e4905
Petru Zdroba <petru.zdroba@gmail.com>
2025-07-28 15:24:09 +0300

    changed my os

--- techtreck-learning/backend/config/settings.py
+++ techtreck-learning/backend/config/settings.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv #type: ignore
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
