18482dfe5604f4d46ffd963dccb785777fd8cdfd
Petru Zdroba <petru.zdroba@gmail.com>
2025-07-30 12:09:10 +0300

    trying to fix azure

--- techtreck-learning/backend/config/settings.py
+++ techtreck-learning/backend/config/settings.py
@@ -1,15 +1,19 @@
 from pathlib import Path
 from datetime import timedelta
-from dotenv import load_dotenv #type: ignore
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+
+if os.environ.get("DJANGO_SECRET_KEY") is None:
+    # We're probably local dev, so import env.py to set env vars
+    import env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
@@ -18,45 +22,45 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").sp
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "rest_framework",
-    "rest_framework_simplejwt",
-    "corsheaders",
-    "core.apps.CoreConfig",
-    "api.apps.ApiConfig",
+"django.contrib.admin",
+"django.contrib.auth",
+"django.contrib.contenttypes",
+"django.contrib.sessions",
+"django.contrib.messages",
+"django.contrib.staticfiles",
+"rest_framework",
+"rest_framework_simplejwt",
+"corsheaders",
+"core.apps.CoreConfig",
+"api.apps.ApiConfig",
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+"corsheaders.middleware.CorsMiddleware",
+"django.middleware.security.SecurityMiddleware",
+"django.contrib.sessions.middleware.SessionMiddleware",
+"django.middleware.common.CommonMiddleware",
+"django.middleware.csrf.CsrfViewMiddleware",
+"django.contrib.auth.middleware.AuthenticationMiddleware",
+"django.contrib.messages.middleware.MessageMiddleware",
+"django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
+{
+"BACKEND": "django.template.backends.django.DjangoTemplates",
+"DIRS": [],
+"APP_DIRS": True,
+"OPTIONS": {
+"context_processors": [
+"django.template.context_processors.request",
+"django.contrib.auth.context_processors.auth",
+"django.contrib.messages.context_processors.messages",
+],
+},
+},
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
@@ -66,10 +70,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.environ.get("DJANGO_DB_PATH", BASE_DIR / "db.sqlite3"),
-    }
+"default": {
+"ENGINE": "django.db.backends.sqlite3",
+"NAME": os.environ.get("DJANGO_DB_PATH", BASE_DIR / "db.sqlite3"),
+}
 }
 
 
@@ -77,18 +81,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
+{
+"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+},
+{
+"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+},
+{
+"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+},
+{
+"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+},
 ]
 
 
@@ -116,44 +120,44 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = (
-    False  # Only for development! Configure properly for production
+False  # Only for development! Configure properly for production
 )
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = os.getenv(
-    "DJANGO_CORS_ALLOWED_ORIGINS", "http://localhost:4200,http://127.0.0.1:4200"
-).split(",")
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:4200",
+    "http://localhost:4200",
+]
+CORS_ALLOWED_ORIGINS = os.getenv("DJANGO_CORS_ALLOWED_ORIGINS", "").split(",")
 
 
 # Allow Authorization header
 CORS_ALLOW_HEADERS = [
-    "accept",
-    "accept-encoding",
-    "authorization",
-    "content-type",
-    "dnt",
-    "origin",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
+"accept",
+"accept-encoding",
+"authorization",
+"content-type",
+"dnt",
+"origin",
+"user-agent",
+"x-csrftoken",
+"x-requested-with",
 ]
 
 CORS_EXPOSE_HEADERS = ["authorization"]
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
+"DEFAULT_AUTHENTICATION_CLASSES": (
+"rest_framework_simplejwt.authentication.JWTAuthentication",
+),
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "AUTH_HEADER_TYPES": ("Bearer",),
-    "AUTH_HEADER_NAME": "Authorization",  # default, keep this
-    "SIGNING_KEY": SECRET_KEY,
-    "USER_ID_FIELD": "id",
-    "USER_ID_CLAIM": "user_id",
+"ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+"REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+"AUTH_HEADER_TYPES": ("Bearer",),
+"AUTH_HEADER_NAME": "Authorization",  # default, keep this
+"SIGNING_KEY": SECRET_KEY,
+"USER_ID_FIELD": "id",
+"USER_ID_CLAIM": "user_id",
 }
-CSRF_TRUSTED_ORIGINS = os.environ.get(
-    "AZURE_DOMAIN", "http://localhost:4200,http://127.0.0.1:4200"
-).split(",")
+CSRF_TRUSTED_ORIGINS = [os.environ.get("AZURE_DOMAIN")]
