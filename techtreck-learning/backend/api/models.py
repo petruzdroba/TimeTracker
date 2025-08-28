90725d9a5b0742625e732f4c1aeb7d52ba26e978
Petru Zdroba <petru.zdroba@gmail.com>
2025-08-28 13:46:05 +0300

    added user field to models

--- techtreck-learning/backend/api/models.py
+++ techtreck-learning/backend/api/models.py
@@ -1,8 +1,17 @@
 from django.db import models  # type: ignore
 
 
+class UserAuth(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.email
+
 class UserData(models.Model):
     id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(UserAuth, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     # stored in hours, as they are default and never changing
@@ -17,17 +26,11 @@ class UserData(models.Model):
         return self.email
 
 
-class UserAuth(models.Model):
-    id = models.AutoField(primary_key=True)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=128)
-
-    def __str__(self):
-        return self.email
 
 
 class WorkLog(models.Model):
     id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(UserAuth, on_delete=models.CASCADE)
     work_log = models.JSONField()
 
     def __str__(self):
@@ -36,6 +39,7 @@ class WorkLog(models.Model):
 
 class Vacation(models.Model):
     id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(UserAuth, on_delete=models.CASCADE)
     future_vacation = models.JSONField()
     past_vacation = models.JSONField()
     remaining_vacation = models.IntegerField(default=14)  # in days
@@ -46,6 +50,7 @@ class Vacation(models.Model):
 
 class LeaveSlip(models.Model):
     id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(UserAuth, on_delete=models.CASCADE)
     future_slip = models.JSONField()
     past_slip = models.JSONField()
     remaining_time = models.IntegerField(default=6)  # in time
@@ -56,6 +61,7 @@ class LeaveSlip(models.Model):
 
 class TimerData(models.Model):
     id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(UserAuth, on_delete=models.CASCADE)
     start_time = models.CharField(max_length=100)
     end_time = models.CharField(max_length=100)
     remaining_time = models.IntegerField()  # in ms
