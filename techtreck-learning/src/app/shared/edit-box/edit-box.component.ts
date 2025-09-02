1ef2d2d5f0ad99a43756f96f33ad4988a6be3ed9
Petru Zdroba <petru.zdroba@gmail.com>
2025-09-02 15:09:17 +0300

    refactor: added toolbar title in edit window

--- techtreck-learning/src/app/shared/edit-box/edit-box.component.ts
+++ techtreck-learning/src/app/shared/edit-box/edit-box.component.ts
@@ -1,11 +1,14 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit-box',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatToolbarModule, MatIconModule],
   templateUrl: './edit-box.component.html',
-  styleUrl: './edit-box.component.css',
+  styleUrls: ['./edit-box.component.css'],
 })
 export class EditBoxComponent {
   @Input({ required: true }) isOpen!: boolean;
