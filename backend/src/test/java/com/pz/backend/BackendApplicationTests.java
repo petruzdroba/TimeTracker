27a6b6836baf1b26f206f93bb7aed1da4d97b814
Petru Zdroba <petru.zdroba@gmail.com>
2025-12-17 09:14:55 +0200

    init: SpringBoot Core initial commit

---
+++ backend/src/test/java/com/pz/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pz.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
