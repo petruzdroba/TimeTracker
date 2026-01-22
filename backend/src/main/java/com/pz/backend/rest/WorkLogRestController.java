4f09cea00678236ee2d65c8fb05f6609d7174fd2
Petru Zdroba <petru.zdroba@gmail.com>
2026-01-22 12:07:32 +0200

    feat: added work log endpoint security check

--- backend/src/main/java/com/pz/backend/rest/WorkLogRestController.java
+++ backend/src/main/java/com/pz/backend/rest/WorkLogRestController.java
@@ -8,6 +8,7 @@ import com.pz.backend.exceptions.AlreadyExistsException;
 import com.pz.backend.exceptions.NotFoundException;
 import com.pz.backend.service.WorkLogService;
 import jakarta.validation.Valid;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,16 +31,19 @@ public class WorkLogRestController {
         return workLogService.get(userId);
     }
 
+    @PreAuthorize("@workLogSecurity.canAccessUser(#workLog.userId, authentication)")
     @PostMapping("/worklog")
     public WorkLog addWorkLog(@Valid @RequestBody WorkLogPostRequest workLog) throws AlreadyExistsException {
         return workLogService.post(workLog.userId(), workLog.date(), workLog.timeWorked());
     }
 
+    @PreAuthorize("@workLogSecurity.isOwner(#workLog.id, authentication)")
     @PutMapping("/worklog")
     public WorkLog updateWorkLog(@Valid @RequestBody WorkLogPutRequest workLog) throws NotFoundException {
         return workLogService.put(workLog.id(), workLog.date(),workLog.timeWorked());
     }
 
+    @PreAuthorize("@workLogSecurity.isOwner(#workLogId, authentication)")
     @DeleteMapping("/worklog/{workLogId}")
     public void deleteWorkLog(@PathVariable Long workLogId) throws NotFoundException {
         workLogService.delete(workLogId);
