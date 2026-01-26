e2288d133fa697f66b8d6777d13742649a5dac26
Petru Zdroba <petru.zdroba@gmail.com>
2026-01-26 09:43:10 +0200

    refactor: added some constraints to the sent data edit-user.component.html + UserPutRequest.java

--- backend/src/main/java/com/pz/backend/dto/UserPutRequest.java
+++ backend/src/main/java/com/pz/backend/dto/UserPutRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.pz.backend.entity.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -19,13 +20,16 @@ public record UserPutRequest(
         @Email(message = "Email must be valid")
         String email,
         @NotNull
-        @Min(1)
+        @Min(0)
+        @Max(12)
         Integer workHours,
         @NotNull
         @Min(1)
+        @Max(365)
         Integer vacationDays,
         @NotNull
         @Min(1)
+        @Max(480)
         Integer personalTime,
         @NotNull
         @JsonProperty(required = true)
